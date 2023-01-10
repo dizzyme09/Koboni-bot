0b9f087b6c929eefd4743d25884d69361804c03f
Belvin Shandy Aurora <belshandy1209@gmail.com>
2023-01-10 14:47:35 +0700

    new menu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const {Client, MessageEmbed} = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const bot = new Client();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-dotenv.config({ path: "./config.env"});
+dotenv.config({ path: "./config.env" });
 
 // get token from config.env
 const token = process.env.TOKEN;
@@ -22,7 +22,15 @@ bot.on("message", (msg) => {
 			break;
 		case "info":
 			if (!args[1]) {
-				msg.channel.send("Commands : \n-Author\n-Version");
+				const infoEmbed = new MessageEmbed()
+					.setTitle("Bot Info")
+					.setColor(0x00ff00)
+					.setDescription("Choose the following options: ")
+					.addField("Author", "Bot Author")
+					.addField("Version", "Bot Version")
+					.addField("Bot", "All Information About Bot")
+					.setFooter("Version: " + VERSION);
+				msg.channel.send(infoEmbed);
 				break;
 			} else {
 				if (args[1] === "Author") {
@@ -31,20 +39,31 @@ bot.on("message", (msg) => {
 				} else if (args[1] === "Version") {
 					msg.channel.send("Version " + VERSION);
 					break;
+				} else if (args[1] === "Bot") {
+					const botInfo = new MessageEmbed()
+						.setTitle("Bot Info")
+						.setColor(0x00ff00)
+						.setDescription("a bot named koboni that will help your server with her sloopy commands!")
+						.addField("Author", "Dizzyme09", true)
+						.addField("Version", VERSION, true);
+					msg.channel.send(botInfo);
+					break;
 				} else {
 					msg.channel.send("Invalid Argument");
 					break;
 				}
 			}
-			case "embed":
-				const embed = new MessageEmbed()
-				.setTitle("Bot Info")
+		case "help":
+			const helpEmbed = new MessageEmbed()
+				.setTitle("Bot Commands")
 				.setColor(0x00ff00)
-				.addField("Name: ", "Koboni")
-				.addField("Author: ", "Dizzyme09")
-				.addField("Version: ", VERSION);
-				msg.channel.send(embed);
-				break;
+				.addField("PREFIX", PREFIX)
+				.addField("help", "Bot Commands")
+				.addField("ping", "Ping bot")
+				.addField("info", "Info About Bot")
+				.setFooter("Version: " + VERSION);
+			msg.channel.send(helpEmbed);
+			break;
 	}
 });
 
